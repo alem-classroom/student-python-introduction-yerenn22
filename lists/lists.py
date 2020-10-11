6d7971e707aaef23a407d1fb96018ba5bcdc67a7
Yeren Kalibek <yerenn22@gmail.com>
2020-10-12 00:54:40 +0600

    feat: add "lists" task solution

--- lists/lists.py
+++ lists/lists.py
@@ -1,17 +1,23 @@
 def size_of_list(list):
-    # return size of list
+    return len(list)
 
 def add_elem_to_list(list, elem):
-    # add elem to list and return the list
+    list.append(elem)
+    return list
 
 def delete_elem_from_list(list, index = -1):
-    # delete element from list, such that its index is index
+    list.pop(-1)
+    return list
 
 def count_elements_in_list(list, x):
-    # count elements in the list that are equal to x and return the count
+    counter = 0
+    for i in range(len(list)):
+        if list[i] == x:
+            counter += 1
+    return counter
 
 def sort_list(list):
-    # return sorted list
+    list.sort()
 
 def reverse(list):
-    # return reversed list
+    return list.reverse()
